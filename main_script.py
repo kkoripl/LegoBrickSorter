7fe345182e7ce0b55bea8311d0abb6f909d6f6af
droalex <44495189+droalex@users.noreply.github.com>
2019-05-30 07:50:30 +0200

    Add files via upload
    removal_coef validation

--- main_script.py
+++ main_script.py
@@ -4,9 +4,9 @@ from layers_trainable_modes import LayersTrainableMode
 from draw_utils import draw_data as dd
 import params_validator as pv
 
-#model_creator = ModelCreator()
-#model_creator.load_base_pretrained_model()  # załadowanie wytrenowanego już modelu
-#model_creator.read_prepared_data()  # odczytanie odpowiednio podzielonych danych
+model_creator = ModelCreator()
+model_creator.load_base_pretrained_model()  # załadowanie wytrenowanego już modelu
+model_creator.read_prepared_data()  # odczytanie odpowiednio podzielonych danych
 
 # ZADANIE 1
 if AppParams.layers_trainable_mode is LayersTrainableMode.ONLY_CLASSIF:
@@ -20,13 +20,13 @@ elif AppParams.layers_trainable_mode is LayersTrainableMode.FROM_LAST_CONV:
 
 # ZADANIE 3 - na razie na zasadzie walidacji, aby znaleźć optymalny współczynnik usuwania warstw
 elif AppParams.layers_trainable_mode is LayersTrainableMode.ALL:
-    removal_coefs = [0.2, 0.4, 0.6]  # współczynniki usuwania warstw
+    removal_coefs = [1]  # współczynniki usuwania warstw
     pv.validate_removal_coef(removal_coefs)
 
 # zapisanie modelu
-model_creator.save_model(
-    f'models/{model_creator.base_model.name}_{AppParams.layers_trainable_mode.name}_{AppParams.epochs}.h5')
-dd.draw_learning_history(model_creator.learn_history)
+# model_creator.save_model(
+#    f'models/{model_creator.base_model.name}_{AppParams.layers_trainable_mode.name}_{AppParams.epochs}.h5')
+# dd.draw_learning_history(model_creator.learn_history)
 
 # do wyboru najlepszej funkcji kosztu
 # pv.validate_loss_functions(['mean_squared_error', 'categorical_hinge', 'categorical_crossentropy'])
--- params_validator.py
+++ params_validator.py
@@ -1,5 +1,6 @@
 from model_creator import ModelCreator
 from draw_utils import draw_data as dd
+from app_params import AppParams
 
 def validate_optimizers(optimizers):
     for optimizer in optimizers:
@@ -17,6 +18,7 @@ def validate_loss_functions(functions):
         model_creator.train_last_fully_connected_layer(final_training_mode=False, loss=func)
         dd.draw_learning_history(model_creator.learn_history, f'Funkcja kary: {func}')
 
+
 def validate_removal_coef(removal_coefs):
     for removal_coef in removal_coefs:
         model_creator = ModelCreator()
@@ -24,3 +26,7 @@ def validate_removal_coef(removal_coefs):
         model_creator.read_prepared_data()
         model_creator.train_whole_convolutional_network(removal_coef, final_training_mode=False)
         dd.draw_learning_history(model_creator.learn_history, f'Wspolczynnik usunietych warstw: {removal_coef}')
+        model_creator.save_model(
+            f'models/{model_creator.base_model.name}_{AppParams.layers_trainable_mode.name}_{AppParams.epochs}_{removal_coef}.h5')
+        dd.draw_learning_history(model_creator.learn_history)
+        del model_creator
